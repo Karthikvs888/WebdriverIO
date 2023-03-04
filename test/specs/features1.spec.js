f1df877baea5de4d161df124bfb9ac39b0e2381a
Karthikvs888 <karthikvs888@gmail.com>
2023-03-04 15:24:29 +0530

    Removed it block

--- test/specs/features1.spec.js
+++ test/specs/features1.spec.js
@@ -271,10 +271,4 @@ describe('Login - ', ()=>{
         await browser.pause(2000);
     })
 
-    it('Pass multiple data from testData - StockEdge Login', async ()=>{
-        Login.open();
-        await Login.loginMultipleData(testData.login)
-        expect(await browser.getTitle()).toEqual('Best Indian Stock Market App for Android and iPhone - StockEdge');
-    })
-
 })
